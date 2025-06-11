7c507caa7846a903a8b155a5201f1af1255e5919
Sumit Gore <123761215+Sumitgore77@users.noreply.github.com>
2025-06-11 11:25:38 +0530

    create saveReg route
    register user in userMaster table

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
 const express = require('express');
+const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
+const bodyParser = require("body-parser");
+app.use(express.urlencoded({ extended: true }));
 let conn = require("./src/config/db");
 
-const app = express();
+
 app.use(express.json());
 
-
 app.get('/', (req, res) => {
-  
       res.render('home.ejs'); 
-  
 });
+
+app.post("/saveReg",(req,res)=>{
+
+  const{name,email,password,confirm_password,contact,type}=req.body;
+
+  if(password==confirm_password){
+
+  conn.query("insert into usermaster values('0',?,?,?,?,?)",[name,email,password,contact,type],(err,result)=>{
+
+    if(err){
+      console.log("Data is not inserted ",err);
+    }
+    else{
+      res.send("Registration Successfully...!");
+    }
+  })
+  }
+  else{
+    console.log("Password is not match");
+  }
+})
+
+app.get("loginUser",(req,res)=>{
+
+  if()
+
+})
 
 app.get("/register",(req,res)=>{
   res.render("register.ejs");
